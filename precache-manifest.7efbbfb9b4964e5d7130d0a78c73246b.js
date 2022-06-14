af0fb6a984e06a604508d785d307e83df1b1ae42
Putra <triputra.anu@gmail.com>
2022-06-14 16:57:22 +0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.7efbbfb9b4964e5d7130d0a78c73246b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "73c3f2d2655a149b6850",
+    "url": "/newhome/static/css/main.ee1074b2.chunk.css"
+  },
+  {
+    "revision": "73c3f2d2655a149b6850",
+    "url": "/newhome/static/js/main.73c3f2d2.chunk.js"
+  },
+  {
+    "revision": "6cd17fd60e28dbd2a951",
+    "url": "/newhome/static/js/1.6cd17fd6.chunk.js"
+  },
+  {
+    "revision": "3af5e78fc4cea57fc3f4",
+    "url": "/newhome/static/js/runtime~main.3af5e78f.js"
+  },
+  {
+    "revision": "c2df7e9bf8dd637350f497af360483ab",
+    "url": "/newhome/static/media/cv.c2df7e9b.pdf"
+  },
+  {
+    "revision": "d7d0e8e38030e84496fd038831c8bbc0",
+    "url": "/newhome/static/media/me.d7d0e8e3.jpg"
+  },
+  {
+    "revision": "de8a482ce916330eac112d9f590adb3f",
+    "url": "/newhome/static/media/nm.de8a482c.jpeg"
+  },
+  {
+    "revision": "1ce973d34acf892a561eb782467aa5b9",
+    "url": "/newhome/static/media/ppm.1ce973d3.png"
+  },
+  {
+    "revision": "e875a8b1659e9529ddcffb5aa7a0c078",
+    "url": "/newhome/static/media/bim.e875a8b1.png"
+  },
+  {
+    "revision": "d9ec7b745a05a10b6901ea23da15062e",
+    "url": "/newhome/static/media/gratyo.d9ec7b74.png"
+  },
+  {
+    "revision": "c071d63bf1d89943bd035cf9815e592c",
+    "url": "/newhome/static/media/zambon.c071d63b.png"
+  },
+  {
+    "revision": "70db4a2c778f886889dc5d9dd76bde76",
+    "url": "/newhome/static/media/ff.70db4a2c.png"
+  },
+  {
+    "revision": "6b9e40704533da82edce44388cdfc77f",
+    "url": "/newhome/static/media/bg-n.6b9e4070.jpg"
+  },
+  {
+    "revision": "5f1d85de3ad876a2a7eb2f02032dc2a2",
+    "url": "/newhome/index.html"
+  }
+];
